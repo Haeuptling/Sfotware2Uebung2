15d57571d1ee99054a387cec2bb06c778a5630f2
Haeuptling <107760711+Haeuptling@users.noreply.github.com>
2022-06-23 08:53:27 +0200

    Add files via upload

---
+++ Software2UebungTeil2/src/main/java/de/hsb/softw2muebase/FilmRepository.java
@@ -0,0 +1,17 @@
+package de.hsb.softw2muebase;
+
+    import org.springframework.data.jpa.repository.JpaRepository;
+    import org.springframework.stereotype.Repository;
+
+<<<<<<< HEAD
+
+@Repository
+interface FilmRepository extends JpaRepository<Film, Integer> {
+}
+=======
+    @Repository
+
+    interface FilmRepository extends JpaRepository<Film, Integer> {
+    }
+>>>>>>> origin/master
+
